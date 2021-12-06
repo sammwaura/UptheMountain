1c1e9b725540901bcbb46988a36af5daf8faf8c8
sammwaura <meshsami27@gmail.com>
2021-12-06 16:18:04 +0300

    modify files

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.fa-bars');
+let navbar =  document.querySelector('.navbar');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
