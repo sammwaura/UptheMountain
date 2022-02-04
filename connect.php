9728aa5c77459112564ef9e04114693a23392f37
sammwaura <meshsami27@gmail.com>
2022-02-04 12:16:08 +0300

    modify files

--- connect.php
+++ connect.php
@@ -7,16 +7,44 @@ $gender = $POST['gender'];
 $find = $POST['find'];
 
 // Database connection
-$conn = new mysqli('localhost', 'root', '', 'test');
-if($conn->connect_error){
-    die('Connection Failed' : '.$conn->connect_error');
-}else{
-    $stmt = $conn->prepare("insert into registration(firstname, lastname, mobile, email, gender, find)
-    values(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssisss",$firstname, $lastname ,$mobile, $email, $gender, $find);
-    $stmt->execute();
-    echo "registration Successfully...";
-    $stmt->close();
-    $conn->close();
+if (!empty($firstname) || !empty($lastname) || !empty($mobile || !empty($email) 
+|| !empty($gender) || !empty($find)) {
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "register";
+
+    // create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+    if(mysqli_connect_error){
+        die('Connect Error('.mysqli_connect_error().')'. mysqli_connect_error());
+    }else{
+        $SELECT = "SELECT email from register Where email = ? Limit 1";
+        $INSERT = "INSERT Into register (firstname, lastname, mobile, email, gender, find) values(?, ?, ?, ?, ?, ?)";
+    // prepare statement
+                $stmt = $conn->prepare($SELECT);
+                $stmt->bind_param("s",$email);
+                $stmt->execute();
+                $stmt->bind_result($email);
+                $stmt->store_result();
+                $rnum = $stmt->num_rows;
+
+                if($rnum==0){
+                    $stmt->close();
+
+                    $stmt = $conn->prepare($INSERT);
+                    $stmt->bind_param("ssisss", $firstname, $lastname, $mobile, $email, $gender, $find);
+                    $stmt->execute();
+                    echo "New record inserted successfully";
+                } else {
+                    echo "Someone already registered using this email";
+                }
+            $stmt->close();
+            $conn->close();
+        }
+    } else {
+        echo "All fields are required";
+        die();
 }
 ?>
