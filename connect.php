b13e9b3df2f81cada8574096acc3f85961511277
sammwaura <meshsami27@gmail.com>
2022-02-07 16:50:12 +0300

    modify

--- connect.php
+++ connect.php
@@ -1,50 +1,34 @@
 <?php
-$firstname = $POST['firstname'];
-$lastname = $POST['lasttname'];
-$mobile = $POST['mobile'];
-$email = $POST['email'];
-$gender = $POST['gender'];
-$find = $POST['find'];
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "register";
 
-// Database connection
-if (!empty($firstname) || !empty($lastname) || !empty($mobile || !empty($email) 
-|| !empty($gender) || !empty($find)) {
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "register";
-
-    // create connection
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-
-    if(mysqli_connect_error){
-        die('Connect Error('.mysqli_connect_error().')'. mysqli_connect_error());
-    }else{
-        $SELECT = "SELECT email from register Where email = ? Limit 1";
-        $INSERT = "INSERT Into register (firstname, lastname, mobile, email, gender, find) values(?, ?, ?, ?, ?, ?)";
-    // prepare statement
-                $stmt = $conn->prepare($SELECT);
-                $stmt->bind_param("s",$email);
-                $stmt->execute();
-                $stmt->bind_result($email);
-                $stmt->store_result();
-                $rnum = $stmt->num_rows;
-
-                if($rnum==0){
-                    $stmt->close();
-
-                    $stmt = $conn->prepare($INSERT);
-                    $stmt->bind_param("ssisss", $firstname, $lastname, $mobile, $email, $gender, $find);
-                    $stmt->execute();
-                    echo "New record inserted successfully";
-                } else {
-                    echo "Someone already registered using this email";
-                }
-            $stmt->close();
-            $conn->close();
-        }
-    } else {
-        echo "All fields are required";
-        die();
+$con = new mysqli($host, $username, $password, $db);
+if(!$con){
+    echo "There are some problems while connecting to the database";
 }
-?>
+// get data from the form
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$mobile = $_POST['mobile'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$find = $_POST['find'];
+$going = $_POST['going'];
+
+// insert to database
+$query = "INSERT INTO `registration`(`firstname`, `lastname`, `mobile`, `email`, `gender`, `find`, `going`) 
+                VALUES ('$firstname', '$lastname', $mobile, '$email', '$gender', '$find', '$going')"
+$insert = mysqli_query($con, $query);
+if(!$insert) {
+    echo "There are some problems while inserting data"
+}
+else{
+    echo "Data inserted";
+}
+
+ ?>
+
+
+
