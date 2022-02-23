c25a6b079bd38828ce71758151756567d24a07e0
sammwaura <meshsami27@gmail.com>
2022-02-23 16:21:27 +0300

    modify files

--- db.php
+++ db.php
@@ -2,13 +2,12 @@
     $servername='localhost';
     $username='root';
     $password='';
-    $dbname = "register";
-    $conn=mysqli_connect($servername,$username,$password,"$dbname");
-      if(!$conn){
-<<<<<<< HEAD
-        die('Could not Connect MySql Server:' .mysqli_error($sql));
-=======
-          die('Could not Connect MySql Server:' .mysql_error());
->>>>>>> 92e8c563d83c871b81a40da689cc8d80310fd665
+    $dbname = "events";
+
+    $conn = mysqli_connect($servername,$username,$password,"$dbname");
+
+    if(!$conn){
+      die('Could not Connect MySql Server:' .mysqli_error($sql));
         }
+        echo "Connected successfully";
 ?>
