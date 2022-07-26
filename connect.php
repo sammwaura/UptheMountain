bf4a83c6fc545d8e0be0b12df3faa4ae3da3fe05
sammwaura <meshsami27@gmail.com>
2022-07-26 12:19:45 +0300

    modify

--- connect.php
+++ connect.php
@@ -4,19 +4,20 @@ include_once 'db.php';
 
 if(isset($_POST['submit']))
 {    
+    $going = $_POST['going'];
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $mobile = $_POST['mobile'];
     $email = $_POST['email'];
     $gender = $_POST['gender'];
     $payment = $_POST['payment'];
+  
 
-
-   $sql = "INSERT INTO mountain (`Id`, `firstname`, `lastname`, `mobile`, `email`, `gender`, `payment`)
-         VALUES ('0', '$firstname', '$lastname', '$mobile', '$email', '$gender', '$payment')";
+   $sql = "INSERT INTO mountain (`going`, `firstname`, `lastname`, `mobile`, `email`, `gender`, `payment`)
+         VALUES ('$going', '$firstname', '$lastname', '$mobile', '$email', '$gender', '$payment')";
          
      if (mysqli_query($conn, $sql)) {
-        echo '<script>alert("New record has been added successfully")</script>';
+        echo '<script>alert("Congratulations! Your registration record has been added successfully!!!!")</script>';
         echo ("<script>location.href='registration.html'</script>");
      } else {
       echo "Error: " . $sql . ":-" .mysqli_error($conn);
